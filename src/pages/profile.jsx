378efec46ff5ce5ab540e4b351f4e67a83ef2983
Afolabi Joseph <maverickoluwatomisin@gmail.com>
2023-10-22 02:53:15 +0100

    user profile page

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -1,12 +1,20 @@
 import Footer from "../components/footer";
 import { UserProfile } from "@clerk/clerk-react";
+import { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const UserProfilePage = () => <UserProfile path="/profile" routing="path" />;
 
 const ProfilePage = () => {
+  useEffect(() => {
+    AOS.init({ duration: 500, offset: 0, once: true });
+  }, []);
   return (
     <main>
-      <UserProfile />
+      <article data-aos="zoom-out">
+        <UserProfile />
+      </article>
       <Footer type={"profile"} />
     </main>
   );
